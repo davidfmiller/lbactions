f90767f78a33c793a9612492c9626a27de43916f
David Miller <davidfmiller@me.com>
2016-04-11 12:15:18 -0700

    make data uri, tweak show in new finder window

---
+++ Make Data URI.lbaction/Contents/Scripts/default.swift
@@ -0,0 +1,45 @@
+#!/usr/bin/env swift
+
+import Foundation
+import Cocoa
+
+let arguments = Array(Process.arguments[1 ..< Process.arguments.count])
+
+for path in arguments
+{
+
+    let data = NSData(contentsOfFile: path)
+    let url = NSURL(fileURLWithPath: path)
+
+    var mime = ""
+    let pathExtension = url.pathExtension!
+    
+    switch(pathExtension)
+    {
+        case "pdf":
+            mime = "application/pdf"
+        case "png":
+            mime = "image/png"
+            break;
+        case "jpg":
+            mime = "image/jpeg"
+        case "jpeg":
+            mime = "image/jpeg"
+        case "gif":
+            mime = "image/png"
+            break;
+        case "css":
+            mime = "text/css"
+        case "js":
+            mime = "application/javascript"
+        case "json":
+            mime = "application/javascript"
+        default:
+            mime = "text/plain"
+        
+    }
+
+    let base64String = "data:" + mime + ";charset=UTF-8;base64," + data!.base64EncodedStringWithOptions([])
+    print(base64String)
+
+}
