7df18b71b4aa414caef50c1831f15b7bc8ec573a
David Miller <davidfmiller@me.com>
2016-08-23 16:01:11 -0700

    update url

--- Get URL.lbaction/Contents/Scripts/default.swift
+++ Get URL.lbaction/Contents/Scripts/default.swift
@@ -1,30 +1,38 @@
 #!/usr/bin/env swift
 
-// LaunchBar Action Script
-
 import Foundation
 
-
 let arguments = Array(Process.arguments[1 ..< Process.arguments.count])
+let fileManager = NSFileManager.defaultManager()
+
+let stderr = NSFileHandle.fileHandleWithStandardError()
+
 for arg in arguments
 {
     let url = NSURL(fileURLWithPath: arg)
     
-    do {
-        
-        let doc = try NSXMLDocument(contentsOfURL: url, options: Int(NSXMLDocumentContentKind.XMLKind.rawValue));
-        
-        let root = doc.rootElement()!;
-        let nodes = try root.nodesForXPath("./dict/string")
-        
-        for i in nodes
+    var err: NSError?
+    
+    if !url.checkResourceIsReachableAndReturnError(&err)
+    {
+        stderr.writeData("🚫  file `\(arg)` doesn't exist".dataUsingEncoding(NSUTF8StringEncoding)!)
+        continue;
+    }
+    
+    var val = "\(url.absoluteURL)"
+    
+    if url.pathExtension == "webloc"
+    {
+        do {
+            let doc = try NSXMLDocument(contentsOfURL: url, options: Int(NSXMLDocumentContentKind.XMLKind.rawValue));
+            let nodes =  try doc.rootElement()!.nodesForXPath("./dict/string")
+            val = nodes[0].stringValue!
+        }
+        catch
         {
-            let value = i.childAtIndex(0)!
-            print("\(value.stringValue!)")
+            
         }
     }
-    catch
-    {
-        print(url.absoluteURL)
-    }
+    
+    print(val)
 }
